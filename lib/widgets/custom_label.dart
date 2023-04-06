397d5981fca8195e9cdd79f3fabf3494dfef1aab
Ítalo Dias <italo-2001@hotmail.com>
2023-04-06 10:38:56 -0300

    Implement: custom label

---
+++ lib/widgets/custom_label.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class LoomiCustomLabel extends StatelessWidget {
+  final String? text;
+  final EdgeInsets padding;
+  final Color? backgroundColor;
+  final Widget? sufix;
+  final Widget? prefix;
+  final TextStyle? textStyle;
+  final double width;
+  final double height;
+  final BorderRadius borderRadius;
+  final BoxBorder? border;
+  const LoomiCustomLabel({
+    super.key,
+    this.text,
+    this.padding = const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+    this.backgroundColor,
+    this.sufix,
+    this.prefix,
+    this.textStyle,
+    this.borderRadius = const BorderRadius.all(Radius.circular(5)),
+    this.border,
+    this.width = 78,
+    this.height = 20,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: padding,
+      decoration: BoxDecoration(
+        borderRadius: borderRadius,
+        color: backgroundColor,
+        border: border,
+      ),
+      child: Row(
+        mainAxisAlignment: text != null && (sufix != null || prefix != null)
+            ? MainAxisAlignment.spaceBetween
+            : MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (prefix != null) prefix!,
+          if (text != null)
+            Text(
+              text!,
+              style: textStyle ?? Theme.of(context).textTheme.subtitle2,
+            ),
+          if (sufix != null) sufix!,
+        ],
+      ),
+    );
+  }
+}
