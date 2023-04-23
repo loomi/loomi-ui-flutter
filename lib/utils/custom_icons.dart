9c5f08283e84ed48601296811bb807442b8dd5e3
Ítalo Dias <italo-2001@hotmail.com>
2023-04-23 18:12:38 -0300

    Map: icons

--- lib/utils/custom_icons.dart
+++ lib/utils/custom_icons.dart
@@ -1,17 +1,189 @@
 class CustomIcons {
-  static String homeIcon = _svgParser("home.svg");
-  static String addIcon = _svgParser("plus-square-Regular_1_.svg");
-  static String moreConfig = _svgParser("more_configs_circle.svg");
-  static String subIcon = _svgParser("minus-square-Regular_1_.svg");
+  static String arrowCircleBackOulined = _svgParser("arrow-back.svg");
+  static String arrowCircleFowardOulined = _svgParser("arrow-circle-right.svg");
+  static String arrowDown = _svgParser("arrow-down.svg");
+  static String arrowBackOulined = _svgParser("arrow-back.svg");
+  static String arrowDownRegular = _svgParser("arrow-down-regular.svg");
+  static String arrowLeftRegular = _svgParser("arrow-left-regular.svg");
+  static String arrowRightRegular = _svgParser("arrow-right-regular.svg");
+  static String arrowUpRegular = _svgParser("arrow-up-regular.svg");
+  static String bellFilled = _svgParser("bell-filled.svg");
+  static String bellRegular = _svgParser("bell-regular.svg");
+  static String bellSlashFilled = _svgParser("bell-slash-filled.svg");
+  static String bellSlashRegular = _svgParser("bell-slash-regular.svg");
+  static String bookmarkFilled = _svgParser("bookmark.svg");
+  static String bookmarkRegular = _svgParser("bookmark-regular.svg");
+  static String calendarFilled = _svgParser("calendar-filled.svg");
+  static String calendarRegular = _svgParser("calendar-regular.svg");
+  static String cameraFilled = _svgParser("camera-filled.svg");
+  static String cameraRegular = _svgParser("camera-regular.svg");
+  static String chartPieFilled = _svgParser("chart-pie-filled.svg");
+  static String chartPieRegular = _svgParser("chart-pie-regular.svg");
+  static String checkCircleFilled = _svgParser("check-circle-filled.svg");
+  static String checkCircleRegular = _svgParser("check-circle-regular.svg");
   static String checkIcon = _svgParser("check_icon.svg");
-  static String infoCircleIcon = _svgParser("info-circle-Regular_1_.svg");
-  static String arrowCircleRight = _svgParser("arrow-circle-right.svg");
-  static String settings = _svgParser("settings.svg");
-  static String filter = _svgParser("filter.svg");
-  static String arrowCircleLeft = _svgParser("arrow-circle-left.svg");
+  static String chevronCircleDownFilled =
+      _svgParser("chevron-circle-down-filled.svg");
+  static String chevronCircleDownRegular =
+      _svgParser("chevron-circle-down-regular.svg");
+  static String chevronCircleLeftFilled =
+      _svgParser("chevron-circle-left-filled.svg");
+  static String chevronCircleRightFilled =
+      _svgParser("chevron-circle-right-filled.svg");
+  static String chevronCircleUpFilled =
+      _svgParser("chevron-circle-up-filled.svg");
+  static String chevronCircleUpRegular =
+      _svgParser("chevron-circle-up-regular.svg");
+  static String chevronDownRegular = _svgParser("chevron-down-regular.svg");
+  static String chevronLeftRegular = _svgParser("chevron-left-regular.svg");
+  static String chevronRightRegular = _svgParser("chevron-right-regular.svg");
+  static String chevronUpRegular = _svgParser("chevron-up-regular.svg");
+  static String clockFilled = _svgParser("clock-filled.svg");
+  static String clockRegular = _svgParser("clock-regular.svg");
+  static String commentDotsFilled = _svgParser("comment-dots-filled.svg");
+  static String commentDotsRegular = _svgParser("comment-dots-regular.svg");
+  static String commentFilled = _svgParser("comment-filled.svg");
+  static String commentRegular = _svgParser("comment-regular.svg");
+  static String commentsFilled = _svgParser("comments-filled.svg");
+  static String commentsRegular = _svgParser("comments-regular.svg");
+  static String compassFilled = _svgParser("compass-filled.svg");
+  static String compassRegular = _svgParser("compass-regular.svg");
+  static String copyFilled = _svgParser("copy-filled.svg");
+  static String copyRegular = _svgParser("copy-regular.svg");
+  static String couponFilled = _svgParser("coupon-filled.svg");
+  static String couponRegular = _svgParser("coupon-regular.svg");
+  static String creditCardFilled = _svgParser("credit-card-filled.svg");
+  static String creditCardRegular = _svgParser("credit-card-regular.svg");
+  static String desktopFilled = _svgParser("desktop-filled.svg");
+  static String desktopRegular = _svgParser("desktop-regular.svg");
+  static String discountFilled = _svgParser("discount-filled.svg");
+  static String discountRegular = _svgParser("discount-regular.svg");
+  static String doubtIcon = _svgParser("doubt-icon.svg");
+  static String downloadFilled = _svgParser("download-filled.svg");
+  static String downloadRegular = _svgParser("download-regular.svg");
+  static String envelopeFilled = _svgParser("envelope-filled.svg");
+  static String envelopeRegular = _svgParser("envelope-regular.svg");
+  static String exclamationCircleFilled =
+      _svgParser("exclamation-circle-filled.svg");
+  static String exclamationCircleRegular =
+      _svgParser("exclamation-circle-regular.svg");
+  static String eyeFilled = _svgParser("eye-filled.svg");
+  static String eyeRegular = _svgParser("eye-regular.svg");
+  static String eyeSlashFilled = _svgParser("eye-slash-filled.svg");
+  static String eyeSlashRegular = _svgParser("eye-slash-regular.svg");
+  static String fileFilled = _svgParser("file-filled.svg");
+  static String fileRegular = _svgParser("file-regular.svg");
+  static String filmFilled = _svgParser("film-filled.svg");
+  static String filmRegular = _svgParser("film-regular.svg");
+  static String filterFilled = _svgParser("filter-filled.svg");
+  static String filterRegular = _svgParser("filter-regular.svg");
+  static String flagFilled = _svgParser("flag-filled.svg");
+  static String flagRegular = _svgParser("flag-regular.svg");
+  static String folderFilled = _svgParser("folder-filled.svg");
+  static String folderRegular = _svgParser("folder-regular.svg");
+  static String globeRegular = _svgParser("globe-regular.svg");
+  static String headphonesFilled = _svgParser("headphones-filled.svg");
+  static String headphonesRegular = _svgParser("headphones-regular.svg");
+  static String heartFilled = _svgParser("heart-filled.svg");
+  static String heartRegular = _svgParser("heart-regular.svg");
+  static String home = _svgParser("home.svg");
+  static String homeFilled = _svgParser("home-filled.svg");
+  static String imageFilled = _svgParser("image-filled.svg");
+  static String imageRegular = _svgParser("image-regular.svg");
+  static String inboxFilled = _svgParser("inbox-filled.svg");
+  static String inboxRegular = _svgParser("inbox-regular.svg");
+  static String infoCircleFilled = _svgParser("info-circle-filled.svg");
+  static String infoCircleRegular = _svgParser("info-circle-Regular_1_.svg");
+  static String linkRegular = _svgParser("link-regular.svg");
+  static String lockFilled = _svgParser("lock-filled.svg");
+  static String lockRegular = _svgParser("lock-regular.svg");
+
+  static String logInRegular = _svgParser("log-in-regular.svg");
+  static String logOutRegular = _svgParser("log-out-regular.svg");
+  static String mapFilled = _svgParser("map-filled.svg");
+  static String mapMarkerFilled = _svgParser("map-marker-filled.svg");
+  static String mapMarkerRegular = _svgParser("map-marker-regular.svg");
+  static String mapPinFilled = _svgParser("map-pin-filled.svg");
+  static String mapPinRegular = _svgParser("map-pin-regular.svg");
+  static String mapRegular = _svgParser("map-regular.svg");
+  static String menuRegular = _svgParser("menu-regular.svg");
+  static String microphoneFilled = _svgParser("microphone-filled.svg");
+  static String microphoneRegular = _svgParser("microphone-regular.svg");
+  static String microphoneSlashFilled =
+      _svgParser("microphone-slash-filled.svg");
+  static String microphoneSlashRegular =
+      _svgParser("microphone-slash-regular.svg");
+  static String minusSquareFilled = _svgParser("minus-square-filled.svg");
+  static String minusSquareRegular = _svgParser("minus-square-Regular_1_.svg");
+  static String mobileFilled = _svgParser("mobile-filled.svg");
+  static String mobileRegular = _svgParser("mobile-regular.svg");
+  static String moreCircleFilled = _svgParser("more-h-circle-filled.svg");
+  static String moreCircleRegularHorizontal =
+      _svgParser("more-h-circle-regular.svg");
+  static String moreCircle = _svgParser("more-v-circle.svg");
+  static String moreCircleFilledVertical =
+      _svgParser("more-v-circle-filled.svg");
+  static String packageFilled = _svgParser("package-filled.svg");
+  static String packageRegular = _svgParser("package-regular.svg");
+  static String paperclipRegular = _svgParser("paperclip-regular.svg");
+  static String penFilled = _svgParser("pen-filled.svg");
+  static String penRegular = _svgParser("pen-regular.svg");
+  static String phoneFilled = _svgParser("phone-filled.svg");
+  static String phoneRegular = _svgParser("phone-regular.svg");
+  static String plusSquareFilled = _svgParser("plus-square-filled.svg");
+  static String plusSquareRegular = _svgParser("plus-square-Regular_1_.svg");
   static String question = _svgParser("question.svg");
+  static String questionCircleFilled = _svgParser("question-circle-filled.svg");
+  static String searchRegular = _svgParser("search-regular.svg");
+  static String sendFilled = _svgParser("send-filled.svg");
+  static String sendRegular = _svgParser("send-regular.svg");
+  static String settingsFilled = _svgParser("settings.svg");
+  static String settingsRegular = _svgParser("settings-regular.svg");
+  static String shieldFilled = _svgParser("shield-filled.svg");
+  static String shieldRegular = _svgParser("shield-regular.svg");
+  static String shoppingBasketFilled = _svgParser("shopping-basket-filled.svg");
+  static String shoppingBasketRegular =
+      _svgParser("shopping-basket-regular.svg");
+  static String slidersRegular = _svgParser("sliders-h-regular.svg");
+  static String sortAscendingFilled = _svgParser("sort-ascending-filled.svg");
+  static String sortAscendingRegular = _svgParser("sort-ascending-regular.svg");
+  static String sortDescendingFilled = _svgParser("sort-descending-filled.svg");
+  static String sortDescendingRegular =
+      _svgParser("sort-descending-regular.svg");
+  static String starFilled = _svgParser("star-filled.svg");
+  static String starRegular = _svgParser("star-regular.svg");
+  static String stopwatchFilled = _svgParser("stopwatch-filled.svg");
+  static String stopwatchRegular = _svgParser("stopwatch-regular.svg");
+  static String storeFilled = _svgParser("store-filled.svg");
+  static String storeRegular = _svgParser("store-regular.svg");
+  static String support = _svgParser("support.svg");
+  static String tabletFilled = _svgParser("tablet-filled.svg");
+  static String tabletRegular = _svgParser("tablet-regular.svg");
+  static String tagFilled = _svgParser("tag-filled.svg");
+  static String tagRegular = _svgParser("tag-regular.svg");
+  static String thumbtackFilled = _svgParser("thumbtack-filled.svg");
+  static String thumbtackRegular = _svgParser("thumbtack-regular.svg");
+  static String timesSquareFilled = _svgParser("times-square-filled.svg");
+  static String timesSquareRegular = _svgParser("times-square-regular.svg");
   static String trash = _svgParser("trash.svg");
-  static String bookmark = _svgParser("bookmark.svg");
+  static String trashFilled = _svgParser("trash-filled.svg");
+
+  static String unlockFilled = _svgParser("unlock-filled.svg");
+  static String unlockRegular = _svgParser("unlock-regular.svg");
+  static String uploadFilled = _svgParser("upload-filled.svg");
+  static String uploadRegular = _svgParser("upload-regular.svg");
+  static String userFilled = _svgParser("user-filled.svg");
+  static String userRegular = _svgParser("user-regular.svg");
+  static String usersFilled = _svgParser("users-filled.svg");
+  static String usersRegular = _svgParser("users-regular.svg");
+  static String usersThreeFilled = _svgParser("users-three-filled.svg");
+  static String usersThreeRegular = _svgParser("users-three-regular.svg");
+  static String videoFilled = _svgParser("video-filled.svg");
+  static String videoRegular = _svgParser("video-regular.svg");
+  static String videoSlashFilled = _svgParser("video-slash-filled.svg");
+  static String videoSlashRegular = _svgParser("video-slash-regular.svg");
+  static String volumeUpFilled = _svgParser("volume-up-filled.svg");
+  static String volumeUpRegular = _svgParser("volume-up-regular.svg");
 
   //PATH FUNCTION
   static String _svgParser(String icon) {
