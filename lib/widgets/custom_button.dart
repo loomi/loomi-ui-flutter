87fd6ca4b09a4b5a1c4dae5ea4b284379c6054ed
lucavps <lucasvp611@hotmail.com>
2023-08-21 15:18:06 -0300

    Add click cursor on custom button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:loomi_ui_flutter/widgets/default_loader.dart';
 
 class CustomButton extends StatefulWidget {
@@ -50,77 +51,80 @@ class CustomButton extends StatefulWidget {
 class _CustomButtonState extends State<CustomButton> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onLongPress: widget.onLongPress,
-      onTap: widget.onTap,
-      child: Container(
-        width: widget.buttonWidth,
-        padding: widget.padding,
-        height: widget.buttonHeight,
-        decoration: BoxDecoration(
-          color: widget.backgroundColor ?? Theme.of(context).primaryColor,
-          border: widget.border,
-          borderRadius: BorderRadius.circular(widget.borderRadius!),
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onLongPress: widget.onLongPress,
+        onTap: widget.onTap,
+        child: Container(
+          width: widget.buttonWidth,
+          padding: widget.padding,
+          height: widget.buttonHeight,
+          decoration: BoxDecoration(
+            color: widget.backgroundColor ?? Theme.of(context).primaryColor,
+            border: widget.border,
+            borderRadius: BorderRadius.circular(widget.borderRadius!),
+          ),
+          child: widget.isLoading == true
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize:
+                      widget.expanded ? MainAxisSize.max : MainAxisSize.min,
+                  children: const [
+                    DefaultLoader(),
+                  ],
+                )
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize:
+                      widget.expanded ? MainAxisSize.max : MainAxisSize.min,
+                  children: [
+                    if (widget.prefix != null) widget.prefix!,
+                    if (widget.prefix != null ||
+                        widget.sufix != null ||
+                        widget.counter != null)
+                      Expanded(
+                        child: Container(),
+                      ),
+                    Text(
+                      widget.text,
+                      style: widget.buttonTextStyle ??
+                          Theme.of(context).textTheme.titleLarge,
+                    ),
+                    if (widget.prefix != null ||
+                        widget.sufix != null ||
+                        widget.counter != null)
+                      Expanded(
+                        child: Container(),
+                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        if (widget.counter != null)
+                          CircleAvatar(
+                            backgroundColor: widget.counterBackgroundColor,
+                            radius: widget.counterRadius,
+                            child: Text(
+                              widget.counter.toString(),
+                              style: widget.counterTextStyle ??
+                                  Theme.of(context).textTheme.titleLarge,
+                            ),
+                          ),
+                        if (widget.sufix != null)
+                          ...[
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            widget.sufix!
+                          ].toList(),
+                      ],
+                    )
+                  ],
+                ),
         ),
-        child: widget.isLoading == true
-            ? Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize:
-                    widget.expanded ? MainAxisSize.max : MainAxisSize.min,
-                children: const [
-                  DefaultLoader(),
-                ],
-              )
-            : Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize:
-                    widget.expanded ? MainAxisSize.max : MainAxisSize.min,
-                children: [
-                  if (widget.prefix != null) widget.prefix!,
-                  if (widget.prefix != null ||
-                      widget.sufix != null ||
-                      widget.counter != null)
-                    Expanded(
-                      child: Container(),
-                    ),
-                  Text(
-                    widget.text,
-                    style: widget.buttonTextStyle ??
-                        Theme.of(context).textTheme.titleLarge,
-                  ),
-                  if (widget.prefix != null ||
-                      widget.sufix != null ||
-                      widget.counter != null)
-                    Expanded(
-                      child: Container(),
-                    ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      if (widget.counter != null)
-                        CircleAvatar(
-                          backgroundColor: widget.counterBackgroundColor,
-                          radius: widget.counterRadius,
-                          child: Text(
-                            widget.counter.toString(),
-                            style: widget.counterTextStyle ??
-                                Theme.of(context).textTheme.titleLarge,
-                          ),
-                        ),
-                      if (widget.sufix != null)
-                        ...[
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          widget.sufix!
-                        ].toList(),
-                    ],
-                  )
-                ],
-              ),
       ),
     );
   }
