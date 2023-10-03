9a6d03a03c6789c2aa8aaad62b97ef0085c75bee
Italo Dias <italo-2001@hotmail.com>
2023-10-03 10:18:19 -0300

    Fix: file picker video player bugs

--- lib/widgets/file_picker/components/file_picker_player/file_picker_player.dart
+++ lib/widgets/file_picker/components/file_picker_player/file_picker_player.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
@@ -16,7 +17,8 @@ class FilePickerPlayer extends StatefulWidget {
 
 class _FilePickerPlayerState extends State<FilePickerPlayer> {
   late VideoPlayerController _controller;
-
+  bool shouldShowWidgets = true;
+  late Timer widgetsTimer;
   @override
   void initState() {
     _controller = widget.url.contains("http")
@@ -27,15 +29,31 @@ class _FilePickerPlayerState extends State<FilePickerPlayer> {
             File(widget.url),
           )
       ..initialize().then((value) {
-        _controller.play();
+        //_controller.play();
+        _startTimer();
         setState(() {});
       });
     super.initState();
   }
 
+  _startTimer() {
+    widgetsTimer = Timer.periodic(const Duration(seconds: 10), (timer) {
+      if (shouldShowWidgets) {
+        toggleWidgets();
+      }
+    });
+  }
+
+  toggleWidgets() {
+    setState(() {
+      shouldShowWidgets = !shouldShowWidgets;
+    });
+  }
+
   @override
   void dispose() {
     _controller.pause();
+    widgetsTimer.cancel();
     _controller.dispose();
     super.dispose();
   }
@@ -45,7 +63,43 @@ class _FilePickerPlayerState extends State<FilePickerPlayer> {
     return Stack(
       children: [
         _controller.value.isInitialized
-            ? VideoPlayer(_controller)
+            ? Stack(
+                children: [
+                  VideoPlayer(_controller),
+                  Positioned.fill(
+                    child: GestureDetector(
+                      onTap: toggleWidgets,
+                      child: Container(
+                        width: double.infinity,
+                        height: double.infinity,
+                        color: shouldShowWidgets
+                            ? Colors.black.withOpacity(.5)
+                            : Colors.transparent,
+                        child: shouldShowWidgets
+                            ? Center(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      _controller.value.isPlaying
+                                          ? _controller.pause()
+                                          : _controller.play();
+                                    });
+                                  },
+                                  child: Icon(
+                                    _controller.value.isPlaying
+                                        ? Icons.pause_rounded
+                                        : Icons.play_arrow_rounded,
+                                    color: Colors.white,
+                                    size: 100,
+                                  ),
+                                ),
+                              )
+                            : const SizedBox.shrink(),
+                      ),
+                    ),
+                  ),
+                ],
+              )
             : Positioned.fill(
                 child: Center(
                   child: CircularProgressIndicator.adaptive(
