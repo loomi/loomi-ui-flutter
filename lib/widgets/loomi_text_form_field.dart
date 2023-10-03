d464cc60e3e3ec0420f935e89be608a6e79e6a13
lucavps <lucasvp611@hotmail.com>
2023-10-03 12:58:34 -0300

    Fix operational system on form field

--- lib/widgets/loomi_text_form_field.dart
+++ lib/widgets/loomi_text_form_field.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -127,9 +128,11 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
               children: [
                 Expanded(
                   child: TextFormField(
-                    onEditingComplete: () => Platform.isIOS
-                        ? FocusScope.of(context).unfocus()
-                        : FocusScope.of(context).nextFocus(),
+                    onEditingComplete: kIsWeb
+                        ? null
+                        : () => Platform.isIOS
+                            ? FocusScope.of(context).unfocus()
+                            : FocusScope.of(context).nextFocus(),
                     onFieldSubmitted: widget.onFieldSubmitted,
                     controller: widget.textEditingController,
                     onChanged: widget.onChanged,
